7cfe71b94bf87734f1c7692bf5cd045c411695de
riyachhillar <59168103+riyachhillar@users.noreply.github.com>
2019-12-26 17:01:20 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,46 @@
+function dropFunc() {
+	myfunc();
+	var elements = document.getElementsByTagName('select')
+	var count = 0;	
+for(var i = 0;i<elements.length;i++)
+{
+	if(elements[i].selected)
+		count++;
+	
+		
+}
+if(count==0)
+{
+	document.getElementById("id2").innerHTML = "Select one option";
+	return false;
+}
+else
+{document.getElementById("id1").innerHTML = "";
+    return true;
+}}
+	
+	
+	
+function myfunc(){
+	
+	
+var elements = document.getElementsByName("gender");
+var count = 0;	
+for(var i = 0;i<elements.length;i++)
+{
+	if(elements[i].checked)
+		count++;
+	
+		
+}
+if(count==0)
+{
+	document.getElementById("id1").innerHTML = "Select one radio button";
+	return false;
+}
+else
+{document.getElementById("id1").innerHTML = "";
+    return true;
+}}
+
+
