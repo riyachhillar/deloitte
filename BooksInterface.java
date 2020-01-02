defba40c5a4a5dfcc8eb921ebc38ba1593b549cf
riyachhillar <59168103+riyachhillar@users.noreply.github.com>
2020-01-02 18:20:04 +0530

    Delete BooksInterface.java

--- BooksInterface.java
+++
@@ -1,8 +0,0 @@
-package com.deloitte.library.services;
-
-import com.deloitte.library.model.Books;
-
-public interface BooksInterface {
- public void insertBook(String bookName, String author, int price);
-
-}
